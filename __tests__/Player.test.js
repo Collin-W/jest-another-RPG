35dd57e2bc383b09bf7dd0033fb93108e68bb1a2
Collin-W <whalencollin@gmail.com>
2021-10-21 09:15:52 -0500

    player obj almost done

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -37,3 +37,32 @@ test('getsinventory from player or returns false', () => {
     expect(player.getInventory()).toEqual(false);
 });
 
+test('get players health val', () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+
+test('check if alive', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
+  });
